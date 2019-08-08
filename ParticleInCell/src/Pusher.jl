57075fc1ba29f08e22ada48bd5ba1fbb132b1c87
Bartek Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-08-08 22:28:36 +0200

    Add UInt32 uid for Paraview Particla Tracker (UInt64 works incorrectly :/)

--- ParticleInCell/src/Pusher.jl
+++ ParticleInCell/src/Pusher.jl
@@ -1,19 +1,36 @@
 module Pusher
 export push_particles!
-
+import Diagnostics
 struct BorisPusher end
 
 function create_boris_pusher()
   return BorisPusher()
 end
 
+struct ParticleVectorData <: Diagnostics.DiagnosticData
+  x :: Array{Float64,1}
+  y :: Array{Float64,1}
+  u :: Array{Float64,1}
+  v :: Array{Float64,1}
+  w :: Array{Float64,1}
+ id :: Array{UInt64,1}
+ it :: Integer
+end
+
+import PlotVTK: pvd_add_timestep, scatter
+Diagnostics.save_diagnostic(dname::String, d::ParticleVectorData, cname::String, c::Any, it::Integer) =
+  pvd_add_timestep(c, scatter(d.x, d.y, dname, dname => (d.u, d.v, d.w), "uuid" => (d.id,), it=it, save=false), it)
+
 function push_particles!(::BorisPusher, part, E, Δt)
-  np = part.np
+  np, id = part.np, part.id
   x, v, q, m = part.x, part.v, part.q, part.m
   F = q*E # Lorentz force, F=qE
   a = F/m # acceleration, F=ma
 
   v[1:np,:] .= v[1:np,:] .+ Δt * a[1:np,:]
   x[1:np,:] .= x[1:np,:] .+ Δt * v[1:np,:]
+
+  Diagnostics.register_diagnostic("pv"*part.name, ParticleVectorData(x[1:np,1], x[1:np,2], v[1:np,1], v[1:np,2], zeros(np), id[1:np], 1))
+  Diagnostics.register_diagnostic("pE"*part.name, ParticleVectorData(x[1:np,1], x[1:np,2], E[1:np,1], E[1:np,2], zeros(np), id[1:np], 1))
 end
 end
--- ParticleInCell/src/pic/macroparticle.jl
+++ ParticleInCell/src/pic/macroparticle.jl
@@ -5,21 +5,33 @@ mutable struct Species
   q :: Float64 # charge
   name::String
   np2c :: Int64 # number of particles per macroparticle
-  np::Int64     # current number of particles
+  np :: Int64     # current number of particles
+  id :: AbstractArray{UInt32,1}   # identifier
 end
 
-Species(name::String, N::Int64) = Species(zeros(N,2), zeros(N,2), 0, 1, name, 1, 0)
+particle_uuids(N::Int64) = collect(1:UInt32(N))
+#particle_uuids(N::Int64) = [rand(UInt32) for i in 1:N]
+
+Species(x::AbstractArray{Float64,2},
+        v::AbstractArray{Float64,2},
+        m::Float64, q::Float64, name::String,
+        np2c::Int64, np::Int64) = Species(x, v, m, q, name, np2c, np, particle_uuids(np))
+Species(name::String, N::Int64) = Species(zeros(N,2), zeros(N,2), 0, 1, name, 1, 0, particle_uuids(N))
 
 function remove!(sp, i::Int64)
   np = sp.np
   sp.x[i,:] .= sp.x[np,:]
   sp.v[i,:] .= sp.v[np,:]
+  sp.id[i]   = sp.id[np]
   sp.np = np - 1
 end
 
 function add!(src, dst)
-  rn = (1 + dst.np):(dst.np + src.np)
-  dst.x[rn,:] .= src.x
-  dst.v[rn,:] .= src.v
-  dst.np += src.np
+  if src.np > 0
+    rn = (1 + dst.np):(dst.np + src.np)
+    dst.x[rn,:] .= src.x
+    dst.v[rn,:] .= src.v
+    # dst has already correct ID numbers
+    dst.np += src.np
+  end
 end
