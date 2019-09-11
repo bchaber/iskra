85b30108eff01697054e8b0260a615b94e9bd54e
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-09-11 10:47:22 +0200

    Two simple test case: 01 for kinetic particles, 02 for fluid species.

---
+++ problem/01_single_electron.jl
@@ -0,0 +1,61 @@
+import Random
+Random.seed!(0)
+############################################
+include("configuration.jl")
+config = Config()
+############################################
+include("units_and_constants.jl")
+############################################
+nx = 20         # number of nodes in x direction
+ny = 20         # number of nodes in y direction
+ts = 200        # number of time steps
+Δh = 5cm        # cell size
+Δt = 20ns       # time step
+Lx = nx*Δh      # domain length in x direction
+Ly = ny*Δh      # domain length in y direction
+############################################
+xs, ys = 0m:Δh:Lx, 0m:Δh:Ly
+sx, sv = [0 Lx; 0 Ly], [0 0; 0 0]
+e  = create_kinetic_species("e-", 20_000,-1qe, 1me, 100_000)
+γe = create_gamma_ionization_source( e, 1/Δt, sx, sv)
+
+import RegularGrid, FiniteDifferenceMethod, ParticleInCell
+config.grid    = RegularGrid.create_uniform_grid(xs, ys)
+config.cells   = RegularGrid.create_staggered_grid(config.grid)
+config.solver  = FiniteDifferenceMethod.create_poisson_solver(config.grid)
+config.pusher  = ParticleInCell.create_boris_pusher()
+config.species = [e]
+############################################
+nx, ny = size(config.grid)
+mx, my = size(config.cells)
+εr  = ones(mx, my, 1)
+bcs = zeros(Int8, nx, ny)
+bcs[ nx,  1] = 1
+bcs[ nx, ny] = 2
+config.cells["εr"] = εr
+set_permittivity(εr)
+add_electrode(bcs .== 1, +1V)
+add_electrode(bcs .== 2, -1V)
+############################################
+import ParticleInCell
+import Diagnostics
+
+function ParticleInCell.enter_loop()
+  Diagnostics.open_container("problem-field")
+  Diagnostics.open_container("problem-particle")
+end
+
+function ParticleInCell.after_loop(it)
+  Diagnostics.save_diagnostic("E",   "problem-field",   it)
+  Diagnostics.save_diagnostic("ϕ",   "problem-field",   it)
+  Diagnostics.save_diagnostic("ne-", "problem-field",   it)
+  Diagnostics.save_diagnostic("pve-","problem-particle",it)
+end
+
+function ParticleInCell.exit_loop()
+  Diagnostics.close_container("problem-field")
+  Diagnostics.close_container("problem-particle")
+end
+
+ParticleInCell.init(γe, Δt)
+@time ParticleInCell.solve(config, Δt, ts, ε0)
---
+++ problem/02_fluid_ions.jl
@@ -0,0 +1,58 @@
+import Random
+Random.seed!(0)
+############################################
+include("configuration.jl")
+config = Config()
+############################################
+include("units_and_constants.jl")
+############################################
+nx = 20         # number of nodes in x direction
+ny = 20         # number of nodes in y direction
+ts = 200        # number of time steps
+Δh = 5cm        # cell size
+Δt = 5000ns     # time step
+Lx = nx*Δh      # domain length in x direction
+Ly = ny*Δh      # domain length in y direction
+############################################
+xs, ys = 0m:Δh:Lx, 0m:Δh:Ly
+sx, sv = [0 Lx; 0 Ly], [0 0; 0 0]
+iO  = create_fluid_species("O+", 1.0, 8qe, 8mp)
+import RegularGrid, FiniteDifferenceMethod, ParticleInCell
+config.grid    = RegularGrid.create_uniform_grid(xs, ys)
+config.cells   = RegularGrid.create_staggered_grid(config.grid)
+config.solver  = FiniteDifferenceMethod.create_poisson_solver(config.grid)
+config.species = [iO]
+############################################
+nx, ny = size(config.grid)
+mx, my = size(config.cells)
+xx, yy = config.grid.x, config.grid.y
+iO.n = @. exp(-(xx-Lx/2)^2/0.03Lx -(yy-Ly/2)^2/0.03Ly)
+εr  = ones(mx, my, 1)
+bcs = zeros(Int8, nx, ny)
+bcs[ nx,  1] = 1
+bcs[ nx, ny] = 2
+config.cells["εr"] = εr
+set_permittivity(εr)
+add_electrode(bcs .== 1, +1V)
+add_electrode(bcs .== 2, -1V)
+############################################
+import ParticleInCell
+import Diagnostics
+
+function ParticleInCell.enter_loop()
+  Diagnostics.open_container("problem-field")
+end
+
+function ParticleInCell.after_loop(it)
+  Diagnostics.save_diagnostic("E",   "problem-field",   it)
+  Diagnostics.save_diagnostic("ϕ",   "problem-field",   it)
+  Diagnostics.save_diagnostic("nO+", "problem-field",   it)
+  Diagnostics.save_diagnostic("ΔnO+","problem-field",   it)
+  Diagnostics.save_diagnostic("vO+", "problem-field",   it)
+end
+
+function ParticleInCell.exit_loop()
+  Diagnostics.close_container("problem-field")
+end
+
+@time ParticleInCell.solve(config, Δt, ts, ε0)
