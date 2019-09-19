9d88c3699baab950bce92a497d406ad6bfb05843
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-09-19 09:25:48 +0200

    Add diffusion coefficient.

--- FiniteDifferenceMethod/src/FiniteDifferenceMethod.jl
+++ FiniteDifferenceMethod/src/FiniteDifferenceMethod.jl
@@ -71,7 +71,7 @@ function apply_dirichlet(::PoissonSolver, dofs, value)
     end
 end
 
-function calculate_advection_diffusion(n, v, Δh, Δt)
+function calculate_advection_diffusion(n, D, v, Δh, Δt)
     nx, ny = size(n)
     vx = view(v,:,:,1)
     vy = view(v,:,:,2)
@@ -80,7 +80,7 @@ function calculate_advection_diffusion(n, v, Δh, Δt)
     for i=2:nx-1
       for j=2:ny-1
         # diffusion
-        Δn[i,j] += Δt*1*(n[i-1,j] + n[i,j-1] - 4n[i,j] + n[i+1,j] + n[i,j+1])/Δh^2
+        Δn[i,j] += Δt*D*(n[i-1,j] + n[i,j-1] - 4n[i,j] + n[i+1,j] + n[i,j+1])/Δh^2
         # advection
         Δn[i,j] -= Δt*n[i,j] * (vx[i+1,j] - vx[i-1,j] + vy[i,j+1] - vy[i,j-1])/2Δh
         Δn[i,j] -= Δt*(vx[i,j] * (n[i+1,j] - n[i-1,j]) + vy[i,j] * (n[i,j+1] - n[i,j-1]))/Δh 
