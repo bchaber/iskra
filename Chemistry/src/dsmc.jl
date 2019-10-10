44dc0d339dc3a4ff3cbf349762bc2db10e34ea39
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-10-10 22:14:58 +0200

    Switch to Variable Hard Sphere model.

--- Chemistry/src/dsmc.jl
+++ Chemistry/src/dsmc.jl
@@ -41,6 +41,7 @@ function perform!(collision::DSMC.ElasticCollision, s, t)
 	g = (source.v[s,:] - target.v[t,:])
 	vc_cm = mr1 * source.v[s,:] .+ mr2 * target.v[t,:]
 	vss_inv= .5
+	vss_inv= 1.
 	print("*")
 	if abs(vss_inv - 1.0) < 1e-4
 		B = 2rand() - 1.0
