17383b221f135190bf8f5fe7ddb460745e5add6b
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-10-10 17:10:17 +0200

    Nodes is a vector with cross-section values.

--- Chemistry/src/cross_section.jl
+++ Chemistry/src/cross_section.jl
@@ -1,12 +1,12 @@
 using Interpolations
 
 struct CrossSection
-	nodes :: Array{Float64,2}
-	interpolation 
+	nodes :: Array{Float64,1}
+	interpolation
 end
 
 CrossSection(xs :: AbstractRange, ys :: Array{Float64,1}) =
-	CrossSection(zeros(1,1), CubicSplineInterpolation(xs, ys; extrapolation_bc=.0))
+	CrossSection(ys, CubicSplineInterpolation(xs, ys; extrapolation_bc=.0))
 (σ :: CrossSection)(x :: Float64) = max(0, σ.interpolation(x))
-
+Base.maximum(σ :: CrossSection) = maximum(σ.nodes)
 Base.show(io :: IO, σ :: CrossSection) = print(io, "σ: ")
