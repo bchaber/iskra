056aeca728a987ac2426f7cc26c88f70027f2870
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-11-10 23:18:08 +0100

    Record both xs and ys of interpolation nodes. Find maximum argument for interpolation nodes.

--- Chemistry/src/cross_section.jl
+++ Chemistry/src/cross_section.jl
@@ -1,12 +1,13 @@
 using Interpolations
 
 struct CrossSection
-	nodes :: Array{Float64,1}
+	nodes :: Array{Float64,2}
 	interpolation
 end
 
 CrossSection(xs :: AbstractRange, ys :: Array{Float64,1}) =
-	CrossSection(ys, CubicSplineInterpolation(xs, ys; extrapolation_bc=.0))
+	CrossSection([xs ys], CubicSplineInterpolation(xs, ys; extrapolation_bc=.0))
 (σ :: CrossSection)(x :: Float64) = max(0, σ.interpolation(x))
-Base.maximum(σ :: CrossSection) = maximum(σ.nodes)
+Base.maximum(σ :: CrossSection) = maximum(σ.nodes[:,2])
+Base.argmax(σ :: CrossSection) = σ.nodes[argmax(σ.nodes[:,2]),1]
 Base.show(io :: IO, σ :: CrossSection) = print(io, "σ: ")
