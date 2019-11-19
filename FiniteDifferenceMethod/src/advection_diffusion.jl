a386b611f181252fba7bd53ed143f33cc8144d9e
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2019-11-19 06:24:32 +0100

    Not sure about the spatial step size.

--- FiniteDifferenceMethod/src/advection_diffusion.jl
+++ FiniteDifferenceMethod/src/advection_diffusion.jl
@@ -3,6 +3,7 @@ end
 
 function calculate_advection_diffusion(n, D, v, Δh, Δt)
     nx, ny = size(n)
+    Δx, Δy, ~ = Δh
     vx = view(v,:,:,1)
     vy = view(v,:,:,2)
     Δn = zeros(nx, ny)
@@ -10,10 +11,10 @@ function calculate_advection_diffusion(n, D, v, Δh, Δt)
     for i=2:nx-1
       for j=2:ny-1
         # diffusion
-        Δn[i,j] += Δt*D*(n[i-1,j] + n[i,j-1] - 4n[i,j] + n[i+1,j] + n[i,j+1])/Δh^2
+        Δn[i,j] += Δt*D*(n[i-1,j] + n[i,j-1] - 4n[i,j] + n[i+1,j] + n[i,j+1])/(Δx*Δy)
         # advection
-        Δn[i,j] -= Δt*n[i,j] * (vx[i+1,j] - vx[i-1,j] + vy[i,j+1] - vy[i,j-1])/2Δh
-        Δn[i,j] -= Δt*(vx[i,j] * (n[i+1,j] - n[i-1,j]) + vy[i,j] * (n[i,j+1] - n[i,j-1]))/Δh 
+        Δn[i,j] -= Δt*n[i,j] * (vx[i+1,j] - vx[i-1,j] + vy[i,j+1] - vy[i,j-1])/2Δx
+        Δn[i,j] -= Δt*(vx[i,j] * (n[i+1,j] - n[i-1,j]) + vy[i,j] * (n[i,j+1] - n[i,j-1]))/Δx 
       end
     end
 
