fa6cf6d754cdc9a2b6e4aa9e60da56467c843d43
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-07-18 21:34:24 +0200

    Implement diffusion equations in 1D
    For now without advection.

--- FiniteDifferenceMethod/src/advection_diffusion.jl
+++ FiniteDifferenceMethod/src/advection_diffusion.jl
@@ -1,7 +1,24 @@
 struct AdvectionDiffusionSolver
 end
 
-function calculate_advection_diffusion(n, D, v, Δh, Δt)
+function calculate_advection_diffusion(n::Vector{Float64}, D, v, Δh, Δt)
+    nx, = size(n)
+    Δx, = Δh
+    vx = view(v,:,1)
+    Δn = zeros(nx)
+
+    for i=2:nx-1
+      # diffusion
+      Δn[i] += Δt*D*(n[i-1] - 2n[i] + n[i+1])/Δx^2
+      # assume no advection
+      #Δn[i] -= Δt*n[i] * (vx[i+1] - vx[i-1])/2Δx
+      #Δn[i] -= Δt*(vx[i] * (n[i+1] - n[i-1]))/Δx 
+    end
+
+    return Δn
+end
+
+function calculate_advection_diffusion(n::Matrix{Float64}, D, v, Δh, Δt)
     nx, ny = size(n)
     Δx, Δy = Δh
     vx = view(v,:,:,1)
