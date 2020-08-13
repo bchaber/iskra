26a27e1e18904198762c6981ca2cf85850672c1e
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-08-13 23:41:29 +0200

    Modify gather/scatter functions to work in cylindrical coordinate system
    There is a bug here. In particle_to_grid the u(i,j) is weighted with 1/ΔV, because it is being used for number density (particle_to_grid is fed with number of real particles in macroparticles, and if we divide each by ΔV we get the number density). It won't work for any property other than number density.
    
    If it is resolved, particle_to_grid and grid_to_particle will look the same for all UniformGrids. Also, volume calculation for each particle is time consuming, so some smart precomputed array is needed.

--- ParticleInCell/src/pic/cloud_in_cell.jl
+++ ParticleInCell/src/pic/cloud_in_cell.jl
@@ -7,11 +7,12 @@ function particle_to_grid(part::KineticSpecies{2, V}, grid::CartesianGrid{2}, pu
 
   for p=1:np
     i, j, hx, hy = particle_cell(px, p, grid.Δh)
+    ΔV  = Δx * Δy
     # interpolate charge to nodes
-    u[i  ,j]   += (1.0 - hx) * (1.0 - hy) * pu(p) / (Δx * Δy)
-    u[i+1,j]   +=        hx  * (1.0 - hy) * pu(p) / (Δx * Δy)
-    u[i  ,j+1] += (1.0 - hx) *        hy  * pu(p) / (Δx * Δy)
-    u[i+1,j+1] +=        hx  *        hy  * pu(p) / (Δx * Δy)
+    u[i  ,j]   += (1.0-hx) * (1.0-hy) * pu(p) / ΔV
+    u[i+1,j]   +=     (hx) * (1.0-hy) * pu(p) / ΔV
+    u[i  ,j+1] += (1.0-hx) *     (hy) * pu(p) / ΔV
+    u[i+1,j+1] +=     (hx) *     (hy) * pu(p) / ΔV
   end
 
   return u
@@ -26,10 +27,48 @@ function grid_to_particle(grid::CartesianGrid{2}, part::KineticSpecies{2, V}, u)
   for p=1:np
     i, j, hx, hy = particle_cell(px, p, Δh)
     # gather electric field
-    pu[p,:] = (1.0-hx)*(1.0-hy)* u(i , j  ) + # contribution from (i,j)
-                   hx *(1.0-hy)* u(i+1,j  ) + # (i+1,j)
-              (1.0-hx)*     hy * u(i,  j+1) + # (i,j+1)
-                   hx *     hy * u(i+1,j+1)   # (i+1,j+1)
+    pu[p,:] = (1.0-hx) * (1.0-hy) * u(i , j  ) + # contribution from (i,j)
+                  (hx) * (1.0-hy) * u(i+1,j  ) + # (i+1,j)
+              (1.0-hx) *     (hy) * u(i,  j+1) + # (i,j+1)
+                  (hx) *     (hy) * u(i+1,j+1)   # (i+1,j+1)
+  end
+
+  return pu
+end
+
+function particle_to_grid(part::KineticSpecies{2, V}, grid::AxialGrid{2}, pu) where V
+  nr, nz = size(grid)
+  Δr, Δz = grid.Δh
+  np = part.np
+  px = view(part.x, 1:np, :)
+  u  = zeros(nr, nz)   # distribution of pu on grid
+
+  for p=1:np
+    i, j, hr, hz = particle_cell(px, p, grid.Δh)
+    ΔV  = π * Δz * ((i*Δr)^2 - (i*Δr - Δr)^2)
+    # interpolate charge to nodes
+    u[i  ,j]   += (1.0-hr) * (1.0-hz) * pu(p) / ΔV
+    u[i+1,j]   +=     (hr) * (1.0-hz) * pu(p) / ΔV
+    u[i  ,j+1] += (1.0-hr) *     (hz) * pu(p) / ΔV
+    u[i+1,j+1] +=     (hr) *     (hz) * pu(p) / ΔV
+  end
+
+  return u
+end
+
+function grid_to_particle(grid::AxialGrid{2}, part::KineticSpecies{2, V}, u) where V
+  np = part.np
+  px = view(part.x, 1:np, :)
+  pu = zeros(np, V)
+
+  Δr, Δz = grid.Δh
+  for p=1:np
+    i, j, hr, hz = particle_cell(px, p, grid.Δh)
+    # gather electric field
+    pu[p,:] = (1.0-hr) * (1.0-hz) * u(i , j  ) + # contribution from (i,j)
+                  (hr) * (1.0-hz) * u(i+1,j  ) + # (i+1,j)
+              (1.0-hr) *     (hz) * u(i,  j+1) + # (i,j+1)
+                  (hr) *     (hz) * u(i+1,j+1)   # (i+1,j+1)
   end
 
   return pu
