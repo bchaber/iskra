f22e53846dcab3ae5154207b763d8d7f901dae0b
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-08-13 23:46:48 +0200

    A little hack to visualize RZ datasets
    Right now I haven't implemented OpenPMD standard for simulations in cylindrical coordinate system. So I fake that it is XY simulation instead.

--- Diagnostics/src/openpmd/root.jl
+++ Diagnostics/src/openpmd/root.jl
@@ -9,8 +9,8 @@ ind(d::Unitful.Dimension{:Luminosity})  = 7
 geo(grid::CartesianGrid{1}) = ("x",), "cartesian", 1
 geo(grid::CartesianGrid{2}) = ("x", "y"), "cartesian", 2
 geo(grid::CartesianGrid{3}) = ("x", "y", "z"), "cartesian", 3
-geo(grid::AxialGrid{1})     = ("r",), "spherical", 2
-geo(grid::AxialGrid{2})     = ("r", "z"), "cylindrical", 2
+geo(grid::AxialGrid{1})     = ("r",), "cartesian", 2
+geo(grid::AxialGrid{2})     = ("r", "z"), "cartesian", 2
 current_version() = "git+?"
 current_date() = Dates.format(Dates.now(), "Y/m/d HH:MM")
 
