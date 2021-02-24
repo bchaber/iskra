6451a2a1b7716938490d06a2447bf8a001212048
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-02-24 05:30:33 -0500

    Move LXCat datasets to datasets/

--- Chemistry/src/biagi71-Ar.jl
+++ Chemistry/src/biagi71-Ar.jl
@@ -1,5 +1,5 @@
 import Chemistry: lxcatread, CrossSection
-σ₁ = lxcatread("Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar, Elastic") |> CrossSection
-σ₂ = lxcatread("Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar*(11.55eV), Excitation") |> CrossSection
-σ₃ = lxcatread("Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar*(13.00eV), Excitation") |> CrossSection
-σ₄ = lxcatread("Biagi-7.1.txt", "PROCESS: E + Ar -> E + E + Ar+, Ionization")  |> CrossSection
+σ₁ = lxcatread("datasets/Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar, Elastic") |> CrossSection
+σ₂ = lxcatread("datasets/Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar*(11.55eV), Excitation") |> CrossSection
+σ₃ = lxcatread("datasets/Biagi-7.1.txt", "PROCESS: E + Ar -> E + Ar*(13.00eV), Excitation") |> CrossSection
+σ₄ = lxcatread("datasets/Biagi-7.1.txt", "PROCESS: E + Ar -> E + E + Ar+, Ionization")  |> CrossSection
--- Chemistry/src/phelps-He.jl
+++ Chemistry/src/phelps-He.jl
@@ -1,3 +1,3 @@
 import Chemistry: lxcatread, CrossSection
-σₑ₁ = lxcatread("Phelps.txt", "PROCESS: He+ + He -> , Isotropic") |> Chemistry.CrossSection
-σₑ₂ = lxcatread("Phelps.txt", "PROCESS: He+ + He -> , Backscat")  |> Chemistry.CrossSection	
+σₑ₁ = lxcatread("datasets/Phelps.txt", "PROCESS: He+ + He -> , Isotropic") |> Chemistry.CrossSection
+σₑ₂ = lxcatread("datasets/Phelps.txt", "PROCESS: He+ + He -> , Backscat")  |> Chemistry.CrossSection	
